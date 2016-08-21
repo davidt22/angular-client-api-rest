3361aeb8e05944b6334697ddeafe29cb1093629e
David Teruel <d_teruel@hotmail.com>
2016-08-21 20:52:31 +0200

    Cap 52: añadir 2 componentes para el registro y login en angular

---
+++ app/components/login.component.ts
@@ -0,0 +1,10 @@
+// Importar el núcleo de Angular
+import {Component, OnInit} from '@angular/core';
+
+@Component({
+    selector: 'login',
+    template: '<h1>Formulario de login</h1>'
+})
+ 
+// Clase del componente donde irán los datos y funcionalidades
+export class LoginComponent { }
---
+++ app/components/register.component.ts
@@ -0,0 +1,10 @@
+// Importar el núcleo de Angular
+import {Component, OnInit} from '@angular/core';
+
+@Component({
+    selector: 'register',
+    template: '<h1>Formulario de registro</h1>'
+})
+ 
+// Clase del componente donde irán los datos y funcionalidades
+export class RegisterComponent { }
